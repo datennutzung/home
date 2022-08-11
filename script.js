5eafc08a7fcb12828f42d1c8861c8c917b0b7a0a
Miron Gertis <miron@gertis.de>
2022-08-11 17:05:07 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    /* Toggle log off
+    const c_like = document.querySelector(".console-like");
+
+    function log(message) {
+        let elem = document.createElement("pre");
+        elem.textContent = message;
+        c_like.appendChild(elem);
+    }
+
+    console.log = log;
+
+    //*/
+
+    const form = document.forms[0];
+
+    form.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+        const query = form.query.value;
+        form.query.value = "";
+        if (query == "") {
+            return;
+        }
+        const searchEngine = form.searchEngine.value;
+        const searchString = searchEngine + encodeURIComponent(query);
+        console.log(searchString);
+        window.open(searchString, "_blank").focus();
+    })
+
+    // date and time
+    const clock = document.querySelector(".clock");
+    const date = document.querySelector(".date");
+
+    function timeSetter() {
+        const now = new Date(Date.now())
+        const options = { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" };
+        clock.innerText = now.toLocaleTimeString("de-DE", options)
+    }
+    timeSetter()
+    setInterval(timeSetter, 500)
+
+
+    function dateSetter() {
+        const now = new Date(Date.now())
+        const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+        date.innerText = now.toLocaleDateString("de-DE", options)
+    }
+    dateSetter()
+    setInterval(dateSetter, 3600_000)
+});
